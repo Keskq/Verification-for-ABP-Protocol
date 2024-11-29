532c81ee9f2854060a0cc61a65719449a307af61
koral reuben <149824651+Keskq@users.noreply.github.com>
2024-11-30 00:05:32 +0200

    Update ABP_env.sv

--- ABP_env.sv
+++ ABP_env.sv
@@ -1,7 +1,11 @@
  class apb_env extends uvm_env;
 
-  // Declare agents
+  `uvm_component_utils(abp_env)
+  
+  // Declare agents, coverage, scoreboard
   apb_agent agent;
+  mux_coverage coverage;
+  mux_scoreboard scoreboard;
 
   // Constructor
   function new(string name = "apb_env", uvm_component parent = null);
@@ -14,13 +18,17 @@
     
     // Create agent
     agent = apb_agent::type_id::create("agent", this);
+    coverage = apb_coverage::type_id::create("coverage",this);
+    scoreboard = apb_scoreboard::type_id::create("scoreboard",this);
   endfunction
   
   // Connect phase to connect all agents
   function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
-    
-    // Connect agents if necessary (if agents connect to each other)
+    agent.monitor.ap_mon.connect(coverage.analysis_export);
+    // connection to aport_drv and aport_mon in scoreboard
+    agent.monitor.ap_mon.connect(scoreboard.aport_mon);
+    agent.driver.drv2sb.connect(scoreboard.aport_drv);
   endfunction
 
- endclass
+ endclass:abp_env
